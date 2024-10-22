922f43e0aac7cc3d0a134a64e17248228b168a52
ganuccio <ganuccio@gmail.com>
2024-10-21 18:10:47 -0700

    lab 55

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,36 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 5
+// Author: Gia Nuccio
+// Date: 10/20/24
 
-// Constants
+// Define Variables using let and const
+const make = "Toyota";
+const model = "Prius";
+const color = "Black";
+const year = 2011;
 
-// Functions
+// Calculate the car's age
+const currentYear = new Date().getFullYear(); // dynamically gets the current year
+let age = currentYear - year; 
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Output the car details
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
+// Example Function: Adds two numbers and returns the sum
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
 
+// Main function to control program flow
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  // You can add any other code here that you want to execute when the page loads
+  let sum = addNumbers(5, 7);
+  console.log("Sum of 5 and 7 is: " + sum);
 }
 
-// let's get this party started
+// Let's get this party started
 main();
