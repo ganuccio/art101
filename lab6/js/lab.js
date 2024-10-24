2eecbd164a373e66a28d9fb397b764f4014252af
ganuccio <ganuccio@gmail.com>
2024-10-24 13:52:06 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -19,3 +19,25 @@ function main() {
 
 // let's get this party started
 main();
+//Define Variables
+let myTransportation = ["make","model", "color", "year", "age"];
+
+
+//Creat object
+let myMainRide = {
+  make: "Toyota",
+  model: "Prius",
+  color: "black",
+  year: 2011,
+  age: function() {
+    return 2024 - this.year;
+  }
+};
+
+// 
+console.log(myMainRide.age()); // Output: 13
+
+// Output
+document.writeln("My Main Ride: <pre>", 
+ JSON.stringify(myMainRide, null, '\t'), "</pre>");
+ 
