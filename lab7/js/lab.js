4f848be66dc678243f703e0ffaca66c5bb9bc510
ganuccio <ganuccio@gmail.com>
2024-10-27 18:27:21 -0700

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,26 @@
+// index.js - Functions lab
+// Author: Gia Nuccio
+// Date: 10/25/24
+
+//sortUserName - a function that takes user input and sorts the letters
+// of their name
+
+function sortUserName() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    //split string to array
+    var nameArray = userName.split(' ');
+    console.log("nameArray") =", name Array)";
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort=", nameArraySort);
+    //join array back to a string
+    var nameSorted = namedArraySort.join(' ');
+    console.log("nameSorted=", nameSorted);
+
+    return nameSorted;
+}
+
+//output
+document.writeln("Oh hey I've fixed your name: ",
+    sortUserName(), "</br>");
